1468168fee1ab4c8779d0858afa3bb02c271613f
Xytavious <143834355+Xytavious@users.noreply.github.com>
2025-05-13 17:45:01 +0000

    something somthing

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import csv
+
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+# df.to_string(index=False)
+
+
+# start to something
+data = pd.read_csv('world_population.csv').to_string().strip()
+print(data)
 
 
 
-with open('world_population.csv', 'r') as file:
-    g
+
